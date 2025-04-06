4e4eafc2781c38e957906811e9413c85e2dbc7de
Daman Chhina <dpschhina@gmail.com>
2025-04-06 19:00:04 -0400

    tab to spaces

--- run.sh
+++ run.sh
@@ -18,8 +18,8 @@ rm ~/code/git.conf; ln -s $dir/git.conf ~/code/git.conf
 
 # download recommended powerline10k font
 cd ~/Library/Fonts && { 
-    curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf' -o 'MesloLGS NF Regular.ttf'
-    curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf' -o 'MesloLGS NF Bold.ttf'
-    curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf' -o 'MesloLGS NF Italic.ttf'
-    curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf' -o 'MesloLGS NF Bold Italic.ttf'
-    cd -; }
+  curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf' -o 'MesloLGS NF Regular.ttf'
+  curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf' -o 'MesloLGS NF Bold.ttf'
+  curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf' -o 'MesloLGS NF Italic.ttf'
+  curl -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf' -o 'MesloLGS NF Bold Italic.ttf'
+  cd -; }
