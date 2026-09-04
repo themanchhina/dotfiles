65f232a01acb79197b60ffa96cb7041af659491f
Daman Chhina <dpschhina@gmail.com>
2026-09-03 23:41:11 -0400

    feat(wezterm): configure word hopping with Option+j/k and clear with Cmd+k

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -7,5 +8,39 @@ config.font = wezterm.font("MesloLGS NF")
 config.font_size = 15.0
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
+
+config.keys = {
+  -- Word hopping: Option + j (backward word) and Option + k (forward word)
+  {
+    key = "j",
+    mods = "OPT",
+    action = act.SendString("\x1bb"),
+  },
+  {
+    key = "k",
+    mods = "OPT",
+    action = act.SendString("\x1bf"),
+  },
+  -- Standard Option + Left/Right arrows for word hopping
+  {
+    key = "LeftArrow",
+    mods = "OPT",
+    action = act.SendString("\x1bb"),
+  },
+  {
+    key = "RightArrow",
+    mods = "OPT",
+    action = act.SendString("\x1bf"),
+  },
+  -- Cmd + k to clear scrollback and viewport
+  {
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple({
+      act.ClearScrollback("ScrollbackAndViewport"),
+      act.SendKey({ key = "L", mods = "CTRL" }),
+    }),
+  },
+}
 
 return config
